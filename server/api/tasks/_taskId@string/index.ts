ee072cdd3d6a45529f83b9d1e1f6a2a12d774b39
1F10210096 <s1F102100962@iniad.org>
2023-08-30 23:53:24 +0900

    chore

---
+++ server/api/tasks/_taskId@string/index.ts
@@ -0,0 +1,12 @@
+import type { Task } from '@prisma/client';
+import type { DefineMethods } from 'aspida';
+
+export type Methods = DefineMethods<{
+  patch: {
+    reqBody: Partial<Pick<Task, 'label' | 'done'>>;
+    status: 204;
+  };
+  delete: {
+    status: 204;
+  };
+}>;
